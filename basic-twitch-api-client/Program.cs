64a44e65b278b2de5dec2aacebc46a9cc70f1d3e
sqayner <sqayner@gmail.com>
2020-04-05 22:02:31 +0300

    Update Program.cs

--- basic-twitch-api-client/Program.cs
+++ basic-twitch-api-client/Program.cs
@@ -10,6 +10,61 @@ namespace basic_twitch_api_client
     {
         static void Main(string[] args)
         {
+            string consoleString;
+            while (true)
+            {
+                consoleFirster();
+
+                consoleString = Console.ReadLine().Replace("basic-twitch-api-client>", "");
+
+                string[] consoleChars = consoleString.Split(new Char[] { ' ' });
+                int consoleLength = consoleChars.Length;
+                Console.WriteLine(consoleLength);
+
+                switch (consoleLength)
+                {
+                    case 1:
+                        switch (consoleString)
+                        {
+                            case "settings":
+                                settings();
+                                break;
+                            default:
+                                break;
+                        }
+                        break;
+                    case 2:
+                        switch (consoleString)
+                        {
+                            case "asd ":
+                                Console.WriteLine("asddd");
+                                break;
+                            default:
+                                break;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                consoleString = "";
+            }
+        }
+
+        private static void settings()
+        {
+
+        }
+
+        private static void consoleWriteline(string response)
+        {
+            string firstText = "basic-twitch-api-client>";
+            Console.WriteLine(firstText + response);
+        }
+
+        private static void consoleFirster()
+        {
+            string firstText = "basic-twitch-api-client>";
+            Console.Write(firstText);
         }
     }
 }
